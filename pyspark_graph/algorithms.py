748c2852ce234b6853cddfc1dda340cfd7624646
Joshua Green <aktungmak@gmail.com>
2023-09-08 14:57:57 +0200

    add a few docstrings

--- pyspark_graph/algorithms.py
+++ pyspark_graph/algorithms.py
@@ -235,10 +235,16 @@ class Pregel(Algorithm):
 
 
 class ConnectedComponents(Algorithm):
-    """Identify connected components, disregarding edge direction"""
+    """
+    Identify connected components if the graph is undirected and strongly connected components
+    if the graph is directed.
+
+    :param max_iterations:
+    """
     COMPONENT = "component"
     ALGO_PREGEL = "pregel"
     ALGO_ALTERNATING = "alternating"
+    ALGO_LAPLACIAN = "laplacian"
 
     def __init__(self, max_iterations=10):
         self.max_iterations = max_iterations
@@ -278,16 +284,28 @@ class ConnectedComponents(Algorithm):
             .agg(_min(DST).alias("min")) \
             .select(col(SRC).alias(ID), least(SRC, "min"))
 
+    def _run_laplacian(self, g) -> DataFrame:
+        raise NotImplementedError()
+
 
 class LabelPropagation(Algorithm):
+    """
+    Starting with an initial label (by default the vertex ID if a label column is not
+    provided) each vertex updates its label based on the modal value of its neighbours.
+    This is repeated for a fixed number of iterations but may converge earlier.
+
+    :param label_column: A Column expression defining the initial label. If not specified the
+                         vertex ID is used instead.
+    :param max_iterations: By default 10, may be less if the graph stage converges earler.
+    """
     LABEL = "label"
 
-    def __init__(self, label_column: str = None, max_iterations=10):
+    def __init__(self, label_column: Column = None, max_iterations=10):
         self.label_column = label_column
         self.max_iterations = max_iterations
 
     def run(self, g: Graph):
-        p = Pregel(initial_state=col(ID) if self.label_column is None else col(self.label_column),
+        p = Pregel(initial_state=col(ID) if self.label_column is None else self.label_column,
                    agg_expr=mode(Pregel.MSG),
                    msg_to_src=None if g.directed else col(Pregel.STATE),
                    msg_to_dst=col(Pregel.STATE),
